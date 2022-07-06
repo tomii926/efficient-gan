58540550627b8a4d8be980edbcced9f3ae28f3bf
Eisuke15 <hideyu41691@gmail.com>
2022-07-07 00:53:59 +0900

    update discriminator

--- net.py
+++ net.py
@@ -42,53 +42,55 @@ class Generator(nn.Module):
 
 
 class Discriminator(nn.Module):
-
     def __init__(self, z_dim=20):
-        super(Discriminator, self).__init__()
-
-        # 画像側の入力処理
-        self.x_layer1 = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=4,
-                      stride=2, padding=1),
-            nn.LeakyReLU(0.1, inplace=True))
-        # 注意：白黒画像なので入力チャネルは1つだけ
-
-        self.x_layer2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=4,
-                      stride=2, padding=1),
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 64, 3, stride=1, padding=1),  # b, 64, 28, 28
             nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1, inplace=True))
+            nn.LeakyReLU(),            
+            nn.MaxPool2d(2)  # b, 64, 14, 14
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, stride=1, padding=1),  # b, 128, 14, 14
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),           
+            nn.MaxPool2d(2)  # b, 128, 7, 7
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),  # b, 256, 7, 7
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2),  # b, 256, 3, 3
+            nn.Dropout(0.2)
+        )
 
-        # 乱数側の入力処理
-        self.z_layer1 = nn.Linear(z_dim, 512)
+        # z input
+        self.z_layer1 = nn.Linear(z_dim, 256)  # b, z_dim ==> b, 512
 
-        # 最後の判定
         self.last1 = nn.Sequential(
-            nn.Linear(3648, 1024),
-            nn.LeakyReLU(0.1, inplace=True))
+            nn.Linear(256 * 3 * 3 + 256, 1024),
+            nn.LeakyReLU(0.1, inplace=True),
+        )
 
         self.last2 = nn.Linear(1024, 1)
 
     def forward(self, x, z):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
 
-        # 画像側の入力処理
-        x_out = self.x_layer1(x)
-        x_out = self.x_layer2(x_out)
-
-        # 乱数側の入力処理
         z = z.view(z.shape[0], -1)
-        z_out = self.z_layer1(z)
+        z = self.z_layer1(z)
 
         # x_outとz_outを結合し、全結合層で判定
-        x_out = x_out.view(-1, 64 * 7 * 7)
-        out = torch.cat([x_out, z_out], dim=1)
+        x = x.view(x.size(0), -1)
+        out = torch.cat([x, z], dim=1)
         out = self.last1(out)
 
         feature = out  # 最後にチャネルを1つに集約する手前の情報
         feature = feature.view(feature.size()[0], -1)  # 2次元に変換
 
         out = self.last2(out)
-
         return out, feature
 
 
