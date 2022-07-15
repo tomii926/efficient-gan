5daf7f8d1f2ce23f8b2c2d5d335ff687ffb7669e
Eisuke15 <hideyu41691@gmail.com>
2022-07-15 22:25:26 +0900

    occluded-mnist

--- dataset.py
+++ dataset.py
@@ -1,22 +1,27 @@
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from torchvision.datasets import MNIST
-from tqdm import tqdm
+from torchvision.utils import save_image
 
 
-class PartialMNIST(Dataset):
-    def __init__(self, numbers, train=True, transform=transforms.ToTensor()):
-        self.dataset = MNIST(root='./data', train=train, transform=transform, download=True)
-        dataloader = DataLoader(self.dataset, batch_size=256, shuffle=False, num_workers=2)
-        now = 0
-        self.indices = []
-        print('loading partial MNIST ...')
-        for _, labels in tqdm(dataloader):
-            self.indices += [i for i, label in enumerate(labels, now) if label in numbers]
-            now += len(labels)
+class OccludedMNIST(Dataset):
+    def __init__(self, *args, **kwargs):
+        self.dataset = MNIST(*args, **kwargs)
 
     def __len__(self):
-        return len(self.indices)
+        return len(self.dataset)
 
     def __getitem__(self, index):
-        return self.dataset[self.indices[index]]
+        image, _ = self.dataset[index]
+        for i in range(28):
+            for j in range(28):
+                if (i - 20) ** 2 + (j - 14) ** 2 <= 25:
+                    image[0][i][j] = 0.0
+        return image, _
+
+
+if __name__ == "__main__":
+    o = OccludedMNIST('./data', train=False, transform=transforms.ToTensor(), download=True)
+    ol = DataLoader(o, 64, shuffle=False, num_workers=2)
+    images, _ = iter(ol).__next__()
+    save_image(images, 'occluded.png', pad_value=1, padding=1)
