0931504a298c6533f7421cffed0d5138424dbe4f
Eisuke15 <hideyu41691@gmail.com>
2022-07-15 23:05:03 +0900

    noisy-mnist

--- dataset.py
+++ dataset.py
@@ -20,8 +20,44 @@ class OccludedMNIST(Dataset):
         return image, _
 
 
+class OccludedMNIST(Dataset):
+    def __init__(self, *args, **kwargs):
+        self.dataset = MNIST(*args, **kwargs)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        image, _ = self.dataset[index]
+        for i in range(28):
+            for j in range(28):
+                if (i - 20) ** 2 + (j - 14) ** 2 <= 25:
+                    image[0][i][j] = 0.0
+        return image, _
+
+
+class NoisyMNIST(Dataset):
+    def __init__(self, *args, **kwargs):
+        self.dataset = MNIST(*args, **kwargs)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        image, _ = self.dataset[index]
+        for i in range(28):
+            for j in range(28):
+                if (i * 28 + j) % 10 == 0:
+                    image[0][i][j] = 1.0
+        return image, _
+
+
 if __name__ == "__main__":
     o = OccludedMNIST('./data', train=False, transform=transforms.ToTensor(), download=True)
+    n = NoisyMNIST('./data', train=False, transform=transforms.ToTensor(), download=True)
     ol = DataLoader(o, 64, shuffle=False, num_workers=2)
-    images, _ = iter(ol).__next__()
-    save_image(images, 'occluded.png', pad_value=1, padding=1)
+    nl = DataLoader(n, 64, shuffle=False, num_workers=2)
+    o_images, _ = iter(ol).__next__()
+    n_images, _ = iter(nl).__next__()
+    save_image(o_images, 'occluded.png', pad_value=1, padding=1)
+    save_image(n_images, 'noisy.png', pad_value=1, padding=1)
